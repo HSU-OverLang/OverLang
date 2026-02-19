2343714f59613749aed876729c374f62406c5b8c
Jiwon Lee <150778909+CH4ER1@users.noreply.github.com>
2026-02-19 17:50:52 +0900

    feat: FE Firebase 인증 기능 (#22)
    Co-authored-by: jiwonlee <jiwonlee@jiwonleeui-MacBookAir.local>
    Co-authored-by: rnrzl <cooky0907@gmail.com>

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,9 +1,18 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import App from '../App';
+import { LoginPage } from '@/app/pages/LoginPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '*',
+    element: <Navigate to="/" replace />,
   },
 ]);
